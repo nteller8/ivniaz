172a2d1a2b5aa2bcb89c759b796b3e5d3de6ebb1
Ivanka Del Mastro <ivanka@MacBook-Pro-de-Ivanka.local>
2023-08-28 16:53:08 -0300

    header y footer

---
+++ ivniaz/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modoOscuro: false,
+      textoModoOscuro: "Modo Oscuro",
+    };
+  }
+
+  render(){
+    return(
+      <>
+      Footer
+      </>
+    )
+  }
+
+}
+
+export default Footer;
