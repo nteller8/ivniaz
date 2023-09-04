cd0e288b64d54a5041b76bd1b0b7e36329c60eff
nteller8 <nteller@udesa.edu.ar>
2023-09-04 15:19:36 -0300

    cambios

--- ivniaz/src/components/Header/Header.js
+++ ivniaz/src/components/Header/Header.js
@@ -1,53 +1,12 @@
 import React, { Component } from "react";
 import "./Header.css";
+import "./Navbar.css";
 
-class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      modoOscuro: false,
-      textoModoOscuro: "Modo Oscuro",
-    };
-  }
-
-  componentDidMount() {
-    console.log("Me monte");
-  }
-
-  cambiarModo() {
-    if (this.state.modoOscuro === false) {
-      this.setState({
-        modoOscuro: true,
-        textoModoOscuro: "Modo Claro",
-      });
-    } else {
-      this.setState({
-        modoOscuro: false,
-        textoModoOscuro: "Modo Oscuro",
-      });
-    }
-  }
-
-  componentDidUpdate() {
-    console.log("me actualice");
-  }
-
-  render() {
-    return (
-      <section className={this.state.modoOscuro ? "oscuro" : ""}>
-        <h1>LOGO</h1>
-        <ul>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-        </ul>
-        <button onClick={() => this.cambiarModo()}>
-          {this.state.textoModoOscuro}
-        </button>
-      </section>
-    );
-  }
-}
-
+function Header () {
+  return (
+    <section>
+      
+    </section>
+)}
 export default Header;
+
