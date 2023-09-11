c9f2dfa4a3463e5be5cc5872768ee5269b103273
Ivanka Del Mastro <ivanka@MacBook-Pro-de-Ivanka.local>
2023-09-11 17:06:02 -0300

    peliculas card

--- ivniaz/src/components/PeliculaCard/PeliculaCard.js
+++ ivniaz/src/components/PeliculaCard/PeliculaCard.js
@@ -11,7 +11,7 @@ class PeliculaCard extends Component {
   }
 
   componentDidMount() {
-    let recuperoStorage = localStorage.getItem("favoritos"); 
+    let recuperoStorage = localStorage.getItem("favoritos");
     if (recuperoStorage === null) {
       this.setState({
         textoFavoritos: "Agregar a favoritos",  //chequear esto
@@ -25,49 +25,64 @@ class PeliculaCard extends Component {
 
   add_remove_favs(id) {
     let arrayPeliculasFav = [];
-    let recuperoStorage = localStorage.getItem("favoritos"); 
-    
+    let recuperoStorage = localStorage.getItem("favoritos");
+
     if (recuperoStorage !== null) {
-        arrayPeliculasFav = JSON.parse(recuperoStorage);
-      
-    if (recuperoStorage.includes(id)) { //chequear si no es solo id
-      arrayPeliculasFav = arrayPeliculasFav.filter(peli_id => peli_id !== id)
-      this.setState({
-        textoFavoritos: "Agregar de favoritos",
-      });
+      arrayPeliculasFav = JSON.parse(recuperoStorage);
+
+      if (recuperoStorage.includes(id)) { //chequear si no es solo id
+        arrayPeliculasFav = arrayPeliculasFav.filter(peli_id => peli_id !== id)
+        this.setState({
+          textoFavoritos: "Agregar de favoritos",
+        });
+      }
+
+      else {
+        arrayPeliculasFav.push(id);
+        this.setState({
+          textoFavoritos: "Quitar de favoritos",
+        });
+      }
+
+      let stringDeFavoritos = JSON.stringify(arrayPeliculasFav);
+      localStorage.setItem("favoritos", stringDeFavoritos);
+
     }
 
-    else {
-      arrayPeliculasFav.push (id);
-      this.setState ({
-        textoFavoritos: "Quitar de favoritos",
-      });
-    }
 
-    let stringDeFavoritos = JSON.stringify(arrayPeliculasFav);
-    localStorage.setItem("favoritos", stringDeFavoritos );
+
 
   }
-
-  
   render() {
     return (
       <React.Fragment>
-          <h2 class = "titulo_ppl">Peliculas populares</h2>
 
-          <section class="peliculas_populares"></section>
+        <article className="peliculas_populares">
+          <h3>{this.props.datosPelicula.title}</h3>
+          <Link to={`/peliculas/${this.props.datosPelicula.id}`}> {/* chequear si no faltan : */}
+            <img className="imagenPP" src={`https://image.tmdb.org/t/p/w500/${this.props.datosPelicula.poster_path}`} alt="imagenPelicula" />
+          </Link>
+
+          <Link to={`/peliculas/${this.props.datosPelicula.id}`}>
+
+            <p className="UnaPelicula">
+              Ver más
+            </p>
+          </Link>
+          <p className="UnaPelicula" onClick={() => this.props.borrar(this.props.datosPelicula.id)}>
+            Borrar
+          </p> {/* chequear q onda esto */}
 
 
+          <button className="UnaPelicula" onClick={() => this.add_remove_favs(this.props.datosPelicula.id)}>
+            {this.state.textoFavoritos}
+          </button>
+        </article>
 
-          <h2 class = "titulo_ppl"> Series populares</h2>
-          <section class = "series_populares" ></section>
 
-
-
-
-</React.Fragment>
+      </React.Fragment>
     )
-  
   }
+}
 
 export default PeliculaCard;
