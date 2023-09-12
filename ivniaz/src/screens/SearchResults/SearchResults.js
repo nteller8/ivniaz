647aaa3da5ba7c146625dd80533bed652b514b31
azulbid <azulbid@gmail.com>
2023-09-11 23:09:38 -0300

    search results

---
+++ ivniaz/src/screens/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from "React";
+
+function SearchResults() {
+    return(
+        <React.Fragment>
+            <p> Resultados de Búsqueda</p>
+        </React.Fragment>
+    );   
+}
+
+export default SearchResults
