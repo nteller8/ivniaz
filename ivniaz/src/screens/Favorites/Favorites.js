b09bbbce9f2ae0b4197d8844a0abc2ca44b63027
azulbid <azulbid@gmail.com>
2023-09-13 12:26:13 -0300

    intente hacer favoritos, el screen esta ok pero no se por que no renderiza

--- ivniaz/src/screens/Favorites/Favorites.js
+++ ivniaz/src/screens/Favorites/Favorites.js
@@ -1,21 +1,65 @@
 import React, { Component } from "react";
-
+import PeliculaCard from "../../components/PeliculaCard/PeliculaCard";
 
 class Favorites extends Component {
   constructor() {
     super();
     this.state = {
-      favoritos: [],
+      pelis: [],
     };
   }
 
+  componentDidMount(){
+    let favs = []
+    let recuperoStorage = localStorage.getItem('favs')
+
+        if(recuperoStorage !== null){
+            favs = JSON.parse(recuperoStorage) 
+            let pelisOk = [];
+
+            favs.forEach(IdFav => {
+                let url = `https://api.themoviedb.org/3/movie/${IdFav}?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US&page=1`
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => pelisOk.push(data))
+                    .then(() => this.setState(
+                        {
+                            pelis: pelisOk }
+                            
+                    ))
+                    .catch(error => console.log('El error es' + error))
+            })   
+        }
+  }
+
+  borrar(id){
+    let favs = [];
+    let recuperoStorage = localStorage.getItem('favs')
+    if(recuperoStorage !== null){
+        let favsToArray = JSON.parse(recuperoStorage);
+        favs = favsToArray
+    }
+    if(favs.includes(id)){ 
+        favs = favs.filter(unId => unId !== id);
+        this.setState({
+        pelis: this.state.pelis.filter(unaPeli => unaPeli.id !== id) 
+        })
+    }
+
+    let favsToString = JSON.stringify(favs);
+    localStorage.setItem('favoritos', favsToString);
+
+
+  }
+
   render() {
-    console.log(this.state.favoritos);
+    console.log(this.state.pelis);
     return (
       <React.Fragment>
         
-        <section>
-          <h1 style={{color: "white"}}>Coming soon...</h1>
+        <section className="main-index">
+          <h1 style={{color: "white"}}>Películas favoritas</h1>
+          {this.state.pelis.map((unaPeli, idx) => <PeliculaCard key={unaPeli + idx} datosPeli={unaPeli} borrar={(peliFiltro)=> this.borrar(peliFiltro)} />)}
         </section>
       
       </React.Fragment>
@@ -26,4 +70,3 @@ class Favorites extends Component {
 
 export default Favorites;
 
-//se va a renderizar favs, no se renderiza nada porque falta terminar el componente
