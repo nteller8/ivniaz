15431bea0ba12c053155e3bf27cfa014541ac6b4
nteller8 <nteller@udesa.edu.ar>
2023-09-14 19:35:29 -0300

    arranque de search

---
+++ ivniaz/src/screens/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import Buscador from "../Buscador/Buscador";
+
+class SearchResults extends Component {
+  constructor() {
+    super();
+    this.state = {
+      peliculasencontradas: [],
+     
+    };
+}
+componentDidMount() {
+    fetch
+
+
+}
+}
