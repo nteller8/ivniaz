a5dbe153b5ed71319f6ff61eb98ba412cafc5571
nteller8 <nteller@udesa.edu.ar>
2023-09-15 16:05:50 -0300

    chequear css de searchresults

--- ivniaz/src/screens/SearchResults/SearchResults.js
+++ ivniaz/src/screens/SearchResults/SearchResults.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PeliculaCard from '../PeliculaCard/PeliculaCard';
 
 class SearchResults extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            peliculaBuscada:[],
+        this.state = {
+            peliculaBuscada: [],
         }
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US&query=${this.props.match.params.abuscar}&page=1&include_adult=false`)
             .then(res => res.json())
             .then(data => this.setState({
@@ -18,19 +18,22 @@ class SearchResults extends Component {
             .catch(error => console.log(error))
     }
 
-    render(){    
-        return(
-        <React.Fragment>
-            <h2 className='titulo'>Resultados de Búsqueda</h2> 
-            <div className="recommended_movies">  
-            {this.state.peliculaBuscada.length > 0 ?
-            <section className="main-index">
-            {this.state.peliculaBuscada.map((peli,idx) => <PeliculaCard key={peli + idx} datosPelicula={peli}/>)} 
-            </section>:
-            <h3> Loading...</h3> }
-            </div>
-        </React.Fragment>
-        )}}
+    render() {
+        return (
+            <React.Fragment>
+                <section className="main-index">
+                    <h2 className='titulo'>Resultados de Búsqueda</h2>
+                    {this.state.peliculaBuscada.length > 0 ?
+                        <section className="recommended_series">
+                            {this.state.peliculaBuscada.map((peli, idx) => <PeliculaCard key={peli + idx} datosPelicula={peli} />)}
+                        </section> :
+                        <h3> Loading...</h3>}
+                </section>
+
+            </React.Fragment>
+        )
+    }
+}
 
 
 export default SearchResults
