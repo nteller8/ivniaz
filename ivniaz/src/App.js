27a744869f8789cec1207027f2b4e4c467f1d709
azulbid <azulbid@gmail.com>
2023-09-15 12:11:24 -0300

    intento arreglar sr

--- ivniaz/src/App.js
+++ ivniaz/src/App.js
@@ -6,7 +6,7 @@ import Favorites from './components/Favorites/Favorites';
 import Detail from './screens/Detail/Detail'
 import Home from './screens/Home/Home';
 import NoEncontrada from './screens/NoEncontrada/NoEncontrada';
-import SearchResults from './components/SearchResults.js/SearchResults';
+import SearchResults from './screens/SearchResults/SearchResults';
 import Loader from './screens/Loader/Loader';
 import { Route, Switch } from 'react-router-dom';
 import TodasCarte from "./components/TodasCarte/TodasCarte";
--- ivniaz/src/screens/SearchResults/SearchResults.js
+++ ivniaz/src/screens/SearchResults/SearchResults.js
@@ -1,20 +1,36 @@
-///import React, { Component } from "react";
-///import { Link } from 'react-router-dom';
-//import Buscador from "../Buscador/Buscador";
+import React, {Component} from 'react';
+import PeliculaCard from '../PeliculaCard/PeliculaCard';
 
-{/*class SearchResults extends Component {
-  constructor() {
-    super();
-    this.state = {
-      peliculasencontradas: [],
-     
-    };
-}
-componentDidMount() {
-    fetch
+class SearchResults extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            peliculaBuscada:[],
+        }
+    }
+    
+    componentDidMount(){
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US&query=${this.props.match.params.abuscar}&page=1&include_adult=false`)
+            .then(res => res.json())
+            .then(data => this.setState({
+                peliculaBuscada: data.results,
+            }, () => console.log(data.results)))
+            .catch(error => console.log(error))
+    }
+
+    render(){    
+        return(
+        <React.Fragment>
+            <h2 className='titulo'>Resultados de Búsqueda</h2> 
+            <div className="recommended_movies">  
+            {this.state.peliculaBuscada.length > 0 ?
+            <section className="main-index">
+            {this.state.peliculaBuscada.map((peli,idx) => <PeliculaCard key={peli + idx} datosPelicula={peli}/>)} 
+            </section>:
+            <h3> Loading...</h3> }
+            </div>
+        </React.Fragment>
+        )}}
 
 
-}
-}
-
-{/* DESPUES METEMOS LINK HACIA POPULARES */}
+export default SearchResults
